1e3bca45773f4dad161578ac1641de11c6f1b06c
Shroez <Shamahararodriguez@gmail.com>
2025-08-12 13:48:50 -0700

    added API function

--- javascript.js
+++ javascript.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "32bc38bf8f4b6t979ce0a923eb70o3e8";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
